69afcd6058dbb5f898907cfca8ab4f1dcb31ddba
Mcvoltrudge Rejouis <37338175+MRejouis2016@users.noreply.github.com>
2018-04-05 20:54:14 -0400

    Add files via upload

--- solarsystem.h
+++ solarsystem.h
@@ -1,1 +1,15 @@
-# solarsystem.h
+class Solarsystem
+{
+  private:
+    int num of planets;
+    double radius;
+  
+  public:
+    Solarsystem();
+    Solarsystem(int);
+    void setPlanets();
+    void setRadius();
+    int getPlanets() const;
+    double getRadius() const;
+    double getArea() const;
+}
